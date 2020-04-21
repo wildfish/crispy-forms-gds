8f955d0fa1cf3e1df69d6f962e1000d3d867c1b1
Stuart MacKay <smackay@flagstonesoftware.com>
2020-04-21 14:46:19 +0100

    Added an app for running code from the cookbook.
    Added an app to the demo site with a single form and view so you can
    easily run the code listed in the cookbook.

---
+++ demo/backend/cookbook/forms.py
@@ -0,0 +1,9 @@
+"""
+Replace the contents of this file with the any of the code snippets
+in the cookbook.
+"""
+from django import forms
+
+
+class CookbookForm(forms.Form):
+    pass
--- demo/backend/urls.py
+++ demo/backend/urls.py
@@ -4,4 +4,5 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path(r"", RedirectView.as_view(url="fields/"), name="home"),
     path(r"fields/", include("backend.fields.urls", namespace="fields")),
+    path(r"cookbook/", include("backend.cookbook.urls", namespace="cookbook")),
 ]
