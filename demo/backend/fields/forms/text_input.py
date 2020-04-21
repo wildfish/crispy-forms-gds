f5aa2a008b992201d249f8a659867c238d38c6bb
Stuart MacKay <smackay@flagstonesoftware.com>
2020-04-21 14:11:21 +0100

    Updated demo site to add changes to Field.

--- demo/backend/fields/forms/text_input.py
+++ demo/backend/fields/forms/text_input.py
@@ -2,7 +2,7 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 from crispy_forms_gds.helper import FormHelper
-from crispy_forms_gds.layout import Layout, Submit, Field
+from crispy_forms_gds.layout import Layout, Submit, Field, Size, Fixed, Fluid
 
 
 class TextInputForm(forms.Form):
@@ -33,9 +33,10 @@ class TextInputForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(TextInputForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
+        self.helper.field_label_size = Size.MEDIUM
         self.helper.layout = Layout(
-            Field.fullname("name"),
-            Field.email("email", css_class="govuk-!-width-one-half"),
-            Field.number("age", css_class="govuk-input--width-3"),
+            "name",
+            Field.text("email", field_width=Fluid.ONE_HALF),
+            Field.text("age", field_width=Fixed.THREE),
             Submit("submit", _("Submit")),
         )
