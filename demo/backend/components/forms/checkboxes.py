c219fa8d366495443a3c3ff7f1b13c1ffa34414f
Stuart MacKay <smackay@flagstonesoftware.com>
2020-05-07 14:45:56 +0100

    Removed the single checkbox from the checkboxes form
    Removed the single checkbox as the Design System does not show a
    similar example - presumably because a yes/no radio button is a better
    user experience.

--- demo/backend/components/forms/checkboxes.py
+++ demo/backend/components/forms/checkboxes.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import HiddenInput, MultipleHiddenInput
+from django.forms import MultipleHiddenInput
 
 from crispy_forms_gds.helper import FormHelper
 from crispy_forms_gds.layout import HTML, Field, Layout, Size, Submit
@@ -15,12 +15,6 @@ class CheckboxesForm(forms.Form):
         error_messages={"required": "Enter the ways to contact you"},
     )
 
-    accept = forms.BooleanField(
-        label="I accept the terms of service",
-        help_text="Please read the terms of service and indicate whether you accept them.",
-        error_messages={"required": "You must accept our terms of service"},
-    )
-
     def __init__(self, *args, **kwargs):
         super(CheckboxesForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
@@ -31,7 +25,6 @@ class CheckboxesForm(forms.Form):
                 small=True,
                 hints={"phone": "Select this option only if you have a mobile phone"},
             ),
-            Field.checkboxes("accept"),
             Submit("submit", "Submit"),
         )
 
@@ -40,23 +33,11 @@ class CheckboxesForm(forms.Form):
         choices = dict(self.fields["method"].choices)
         return ", ".join([choices.get(item) for item in value])
 
-    def get_accept(self):
-        value = self.cleaned_data["accept"]
-        return "Yes" if value else "No"
-
     def valid_layout(self):
         self.fields["method"].widget = MultipleHiddenInput()
-        self.fields["accept"].widget = HiddenInput()
         self.helper.layout = Layout(
             "method",
-            "accept",
             HTML.h2("You answered..."),
-            HTML.table(
-                None,
-                [
-                    ("Methods selected:", self.get_method()),
-                    ("Terms accepted:", self.get_accept()),
-                ],
-            ),
+            HTML.table(None, [("Methods selected:", self.get_method())]),
             Submit("continue", "Continue"),
         )
