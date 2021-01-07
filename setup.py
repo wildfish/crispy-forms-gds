63895cf96c2c0adea97fef1f0fb4ffebeed7cc47
Stuart MacKay <smackay@flagstonesoftware.com>
2021-01-07 19:16:15 +0000

    Separate package metadata from tool configuration options
    1. Added options for updating version numbers using with bump2version
    2. Fixed pytest configuration so paths to tests and src are correct
    3. Aligned configuration flake8 and isort to be Black compatible
    4. Expanded list of environments tested with tox to include Django 3.1

--- setup.py
+++ setup.py
@@ -1,5 +1,61 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""
+setup.py
+
+setup() is configured with the project metadata so setup.cfg is used
+primarily for options for the various tools used.
+
+
+"""
+import os
+
 from setuptools import setup
 
-setup()
+
+def read(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as fp:
+        return fp.read()
+
+
+setup(
+    name="crispy-forms-gds",
+    version="0.2.1",
+    description="Django application to add 'django-crispy-forms' layout objects for the GOV.UK Design System.",
+    long_description=read("README.rst"),
+    long_description_content_type="text/x-rst",
+    author="Wildfish",
+    author_email="developers@wildfish.com",
+    keywords="Django, django-crispy-forms, gov.uk, design system",
+    url="https://github.com/wildfish/crispy-forms-gds",
+    packages=[
+        "crispy_forms_gds",
+        "crispy_forms_gds/layout",
+        "crispy_forms_gds/templatetags",
+    ],
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.6",
+    install_requires=[
+        "Django>=2.2",
+        "django-crispy-forms>=1.9.0",
+    ],
+    license="License :: OSI Approved :: BSD License",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
