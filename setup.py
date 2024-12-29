ee7dba0480235096f339afe051b696cefc8dc499
Stuart MacKay <smackay@flagstonesoftware.com>
2024-12-29 17:42:13 +0000

    project: Set minimum python version to 3.8

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=[
         "Django>=3.2",
         "django-crispy-forms>=1.9",
